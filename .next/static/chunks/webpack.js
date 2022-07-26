7e16037205cee7d64ffd4b276e424cff09162a7e
marcoscode404 <viniciusmg99@outlook.com>
2022-07-26 13:28:47 -0400

    adicionando imahem logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9908882adc81461d7a57";
+/******/ 	var hotCurrentHash = "bc357f5f878dde93a881";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/sliders/index.tsx
+++ src/components/sliders/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Button, Flex, Image } from '@chakra-ui/react';
 import { FiMenu } from 'react-icons/fi';
 
 export default function Slides(): JSX.Element {
@@ -8,33 +8,44 @@ export default function Slides(): JSX.Element {
     <header>
       <Flex
         h="20rem"
-        gap="1rem"
+        gap="2rem"
         marginTop=".5rem"
         bg="#161216"
         mt="1.3rem"
       >
         <Box
           bg= "#018C63"
-          w="100%"
+          border="1px solid #2de6ae"
+          w="66%"
           p=".3rem"
           borderRadius="8px"
+          backgroundSize="cover"
         >
-          <h4>Slide Dinamico aqui!</h4>
+            <Image 
+              w="100%" 
+              h="100%"
+              src='imgLogoColab.jpeg'
+            />
         </Box>
         <Box
           display="flex"
           justifyContent="end"
           bg="#015f43"
           border="1px solid #2de6ae"
-          w="3%"
-          height="2rem"
-          p=".3rem"
+          w="30%"
+          height="100%"
+          p=".2rem"
           borderRadius="8px"
         >
-          <FiMenu 
-            width="100%"
-            fontSize="1.5rem"
-          />
+          
+          <Button h="2rem" w="2rem" border="1px solid #2de6ae" bg="#018C63">
+            <FiMenu 
+              fontSize="1.5rem"
+              cursor="pointer"
+              color="white"
+            />
+          </Button>
+          
         </Box>
 
       </Flex>
